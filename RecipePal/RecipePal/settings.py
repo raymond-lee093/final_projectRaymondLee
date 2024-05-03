a0e5ecc04f16eaf26252e7d80d829a9985b7606b
Raymond Lee <raymondlee093@gmail.com>
2024-05-02 23:25:50 -0500

    Established connection to postgresSQL database

--- RecipePal/RecipePal/settings.py
+++ RecipePal/RecipePal/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "RecipePal.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "RecipePal",
+        "USER": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
