72d625e81ec4ccf6cf9465e7210d3b3f9d7be9c2
Raymond Lee <raymondlee093@gmail.com>
2024-05-03 13:42:41 -0500

    Modified url patterns.

--- RecipePal/RecipePal/urls.py
+++ RecipePal/RecipePal/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
-    path("main/", include("main.urls")),
+    path("", include("main.urls")),
     path("admin/", admin.site.urls),
 ]
+
+
