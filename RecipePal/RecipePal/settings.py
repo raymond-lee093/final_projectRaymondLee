37b1deb87004a46f8b5fab207d71f5ca46917451
Raymond Lee <raymondlee093@gmail.com>
2024-05-04 12:49:25 -0500

    Added account app to installed apps.

--- RecipePal/RecipePal/settings.py
+++ RecipePal/RecipePal/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # My app
+    # My apps
     "main.apps.MainConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
