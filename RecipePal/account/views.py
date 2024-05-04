18faaf4e7bf54343b76eef65aa9198c1b452daae
Raymond Lee <raymondlee093@gmail.com>
2024-05-04 15:55:48 -0500

    Created login template. Created login_user view.

--- RecipePal/account/views.py
+++ RecipePal/account/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def login_user(request):
+    context = {}
+    return render(request, "authentication/login.html", context)
