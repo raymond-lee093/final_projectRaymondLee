ec971347cddc0ac2d46419aa94e0e5006047a5a8
Raymond Lee <raymondlee093@gmail.com>
2024-05-04 21:03:00 -0500

    Created login_user view. Added account:login url.

--- RecipePal/account/views.py
+++ RecipePal/account/views.py
@@ -5,4 +5,20 @@ from django.contrib import messages
 
 def login_user(request):
     context = {}
-    return render(request, "authentication/login.html", context)
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # Verify credentials, check against authentication backend
+        # returns user object if credentials are valid
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Login user
+            login(request, user)
+            return redirect("main:index")
+        else:
+            # Return an 'invalid login' error message. Redirects to login page
+            messages.error(request, "Your username or password didn't match. Try again.")
+            return redirect("account:login_user")
+    else:
+        return render(request, "authentication/login.html", context)
+
