ed1b88b6eeb8ffe5e603dcaf1b1e15e2ac050368
Raymond Lee <raymondlee093@gmail.com>
2024-05-05 03:52:19 -0500

    admin.py was created to update admin site.

--- RecipePal/account/admin.py
+++ RecipePal/account/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . models import Account
 
-# Register your models here.
+
+# Due to creating new user model, must tell admin site how to use it
+class AccountAdmin(UserAdmin):
+	# Display these fields in admin site
+	list_display = ('username','email','date_joined', 'last_login', 'is_admin','is_staff')
+	# Fields that are allowed to be searched in admin site
+	search_fields = ('email','username',)
+	# Fields that cannot be changed in admin site
+	readonly_fields = ('id', 'date_joined', 'last_login')
+
+	filter_horizontal = ()
+	list_filter = ()
+	fieldsets = ()
+
+
+# Register new admin with Account model
+admin.site.register(Account, AccountAdmin)
