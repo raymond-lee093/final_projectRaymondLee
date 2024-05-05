e1383ab021882b9f0ee2d5c0fe56184e5ee5b380
Raymond Lee <raymondlee093@gmail.com>
2024-05-05 11:48:17 -0500

    Added password reset and password chang urls.

--- RecipePal/RecipePal/urls.py
+++ RecipePal/RecipePal/urls.py
@@ -19,13 +19,31 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", RedirectView.as_view(url='account/login_user')),
     path("main/", include("main.urls")),
     path("admin/", admin.site.urls),
-    path("account/", include("django.contrib.auth.urls")),
     path("account/", include("account.urls")),
+    # Password reset links
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='password_reset/password_change_done.html'),
+         name='password_change_done'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_reset/password_change.html'),
+         name='password_change'),
+
+    path('password_reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 # Builds a url that tells Django where static and media files hosted in development
