10029e3509cbaa99038f3017749c724999fa95e0
Raymond Lee <raymondlee093@gmail.com>
2024-05-07 00:18:53 -0500

    Modified fields.

--- RecipePal/spoonacular_api/forms.py
+++ RecipePal/spoonacular_api/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class RecipeForm(forms.Form):
     # Fields are required by default
-    query = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': "Enter ingredient(s)", 'autofocus': True}))
+    query = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': "Enter ingredient(s)", 'autofocus': True}))
     cuisine = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': "Enter cuisine type(s)"}))
     intolerances = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': "Enter dietary restriction(s)"}))
     type = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': "Enter meal type(s)"}))
